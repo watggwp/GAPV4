cf77216493f13f9f60d186aa17ffe97e9835a880
thanawat1303 <fquarius13@gmail.com>
2024-12-06 23:16:12 +0700

    set liff

--- app/src/web/farmer/index.js
+++ app/src/web/farmer/index.js
@@ -22,6 +22,6 @@ const id = {
     "signup" : process.env.REACT_APP_LINE_SIGNUP ,
     "house" : process.env.REACT_APP_LINE_HOUSE ,
     "form" : process.env.REACT_APP_LINE_FORM ,
-    "houseList" : process.env.REACT_APP_LINE_HOUSELISE
+    "houses" : process.env.REACT_APP_LINE_HOUSELISE
 }
 ReactDOM.createRoot(document.getElementById('farmer')).render(<MainFarmer socket={socket} Path={Path} idLiff={id[Path]}/>)
