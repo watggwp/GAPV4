2f954fd446cddfe0de57a52d3ded6a78d98a187c
thanawat1303 <fquarius13@gmail.com>
2024-12-21 17:48:08 +0700

    popup

--- app/src/web/farmer/src/content/Factor/InsertFactor.js
+++ app/src/web/farmer/src/content/Factor/InsertFactor.js
@@ -668,7 +668,9 @@ const PopupInsertFactor = ({
   };
 
   const containsHidePopup = useCallback((element , target) => {
-    !element.contains(target) && element.setAttribute("remove","")
+    setTimeout(() => {
+        !element.contains(target) && element.setAttribute("remove","")
+    }, 10);
   } , [])
 
   //
