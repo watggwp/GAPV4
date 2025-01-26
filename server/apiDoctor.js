be1d270f504e342c81fd1eae39ec49a0c92b7b40
thanawat1303 <fquarius13@gmail.com>
2025-01-26 14:30:17 +0700

    report

--- server/apiDoctor.js
+++ server/apiDoctor.js
@@ -3529,55 +3529,56 @@ module.exports = function apiDoctor (app , Database , apifunc , dbpacket , listD
             )
         })
     }
+
     app.post('/api/doctor/data/statistic/get', async (req, res) => {
         let username = req.session.user_doctor;
         let password = req.session.pass_doctor;
-       
+        
         if (username === '' || password === '') {
-          res.redirect('/api/logout');
-          return;
+            res.redirect('/api/logout');
+            return;
         }
-       
+        
         let con = Database.createConnection(listDB);
-       
+        
         try {
-          const auth = await apifunc.auth(con, username, password, res, "doctor");
-          if (auth['result'] === "pass") {
+            const auth = await apifunc.auth(con, username, password, res, "acc_doctor");
+            if (auth['result'] === "pass") {
             con.query(
-              `
+                `
                 SELECT
-                  p.pest_name,
-                  p.type_pest,
-                  COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 1 WEEK) THEN fc.pest_id END) AS total_1_week,
-                  COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 1 MONTH) THEN fc.pest_id END) AS total_1_month,
-                  COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 3 MONTH) THEN fc.pest_id END) AS total_3_months,
-                  COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 6 MONTH) THEN fc.pest_id END) AS total_6_months,
-                  COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 1 YEAR) THEN fc.pest_id END) AS total_1_year
+                    p.pest_name,
+                    p.type_pest,
+                    COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 1 WEEK) THEN fc.pest_id END) AS total_1_week,
+                    COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 1 MONTH) THEN fc.pest_id END) AS total_1_month,
+                    COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 3 MONTH) THEN fc.pest_id END) AS total_3_months,
+                    COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 6 MONTH) THEN fc.pest_id END) AS total_6_months,
+                    COUNT(CASE WHEN fc.date >= DATE_SUB(NOW(), INTERVAL 1 YEAR) THEN fc.pest_id END) AS total_1_year
                 FROM formchemical fc
                 LEFT JOIN pests p ON fc.insect = p.pest_name
                 GROUP BY fc.insect
                 LIMIT 25;
-              `,
-              (err, result) => {
+                `,
+                (err, result) => {
                 if (err) {
-                  dbpacket.dbErrorReturn(con, err, res);
-                  return;
+                    dbpacket.dbErrorReturn(con, err, res);
+                    return;
                 }
-       
+        
                 con.end();
                 res.send(result); // ส่งข้อมูลสรุป pest_name, type_pest และจำนวน pest ตามระยะเวลา
-              }
+                }
             );
-          }
+            }
         } catch (err) {
-          con.end();
-          if (err == "not pass") {
+            con.end();
+            if (err == "not pass") {
             res.redirect('/api/logout');
-          }
+            }
         }
-      });
+    });
 
-      app.get('/api/doctor/data/report/list', async(req, res) => {
+    app.get('/api/doctor/data/report/list', async(req, res) => {
         let username = req.session.user_doctor
         let password = req.session.pass_doctor
         if(username === '' || password === '') {
@@ -3586,28 +3587,28 @@ module.exports = function apiDoctor (app , Database , apifunc , dbpacket , listD
         }
         let con = Database.createConnection(listDB)
         try {
-          const auth = await apifunc.auth(con , username , password , res , "doctor")
+          const auth = await apifunc.auth(con , username , password , res , "acc_doctor")
           if(auth['result'] === "pass") {
             const station = auth['data']['station_data']
     // ดึงข้อมูลเกษตรกรและพืชใน station
-          const farmerQuery = `
+            const farmerQuery = `
                 SELECT
-                  acc_farmer.station,
-                  COUNT(DISTINCT acc_farmer.uid_line) AS total_farmers,
-                  COUNT(DISTINCT formplant.id_farm_house) AS total_plants,
-                  GROUP_CONCAT(DISTINCT formplant.name_plant SEPARATOR ', ') AS plants,
-                  CONCAT(
-                      '[',
-                      GROUP_CONCAT(
-                          DISTINCT CONCAT(
-                              '{"plantName":"', subquery.name_plant, '",' ,
-                              '"id":"', subquery.id, '",' ,
-                              '"farmersCount":', subquery.total_qty, ',' ,
-                              '"duplicateCount":', subquery.duplicate_count, '}'
-                          )
-                      ),
-                      ']'
-                  ) AS plantDetails
+                acc_farmer.station,
+                COUNT(DISTINCT acc_farmer.uid_line) AS total_farmers,
+                COUNT(DISTINCT formplant.id_farm_house) AS total_plants,
+                GROUP_CONCAT(DISTINCT formplant.name_plant SEPARATOR ', ') AS plants,
+                CONCAT(
+                    '[',
+                    GROUP_CONCAT(
+                        DISTINCT CONCAT(
+                            '{"plantName":"', subquery.name_plant, '",' ,
+                            '"id":"', subquery.id, '",' ,
+                            '"farmersCount":', subquery.total_qty, ',' ,
+                            '"duplicateCount":', subquery.duplicate_count, '}'
+                        )
+                    ),
+                    ']'
+                ) AS plantDetails
                 FROM acc_farmer
                 LEFT JOIN housefarm ON acc_farmer.uid_line = housefarm.uid_line
                 LEFT JOIN formplant ON housefarm.id_farm_house = formplant.id_farm_house
@@ -3623,57 +3624,56 @@ module.exports = function apiDoctor (app , Database , apifunc , dbpacket , listD
                 ) AS subquery ON housefarm.id_farm_house = subquery.id_farm_house
                 WHERE acc_farmer.station = ?
                 GROUP BY acc_farmer.station;
-          `;
-          con.query(farmerQuery, [station], (err, farmerStatistics) => {
-              if (err) {
-                  console.error('Error fetching farmer statistics:', err);
-                  res.status(500).json({ status: "error", message: "Database query error" });
-                  return;
-              }
-              console.log('Farmer Statistics:', farmerStatistics);
-    // ดึงรายชื่อหมอพืชสำหรับ station นี้
-              const doctorQuery = `
-                  SELECT id_doctor, fullname_doctor, station_doctor
-                  FROM acc_doctor
-                  WHERE station_doctor = ?;
-              `;
-              con.query(doctorQuery, [station], (err, doctors) => {
-                  if (err) {
-                      console.error('Error fetching doctor data:', err);
-                      res.status(500).json({ status: "error", message: "Database query error" });
-                      return;
-                  }
-                  console.log('Doctors:', doctors);
-    // ส่งผลลัพธ์กลับไป
-                  res.status(200).json({
-                      status: "success",
-                      data: {
-                          farmerStatistics: farmerStatistics.map((stat) => ({
-                              station: stat.station,
-                              totalFarmers: stat.total_farmers,
-                              totalPlants: stat.total_plants,
-                              plants: stat.plants,
-                              plantDetails: JSON.parse(stat.plantDetails || "[]").reduce((prev , curr) => {
-                                  const indexFind = prev.findIndex(({ plantName }) => plantName === curr["plantName"])
-                                  if(indexFind >= 0) {
-                                      prev[indexFind]["farmersCount"] += curr["farmersCount"]
-                                  } else {
-                                      prev.push({
-                                          plantName : curr["plantName"],
-                                          farmersCount : curr["farmersCount"]
-                                      })
-                                  }
-                                  return prev
-                              } , []),
-                          })),
-                          doctors,
-                      },
-                  });
-              });
-          });
+            `;
+            con.query(farmerQuery, [station], (err, farmerStatistics) => {
+                if (err) {
+                    console.error('Error fetching farmer statistics:', err);
+                    res.status(500).json({ status: "error", message: "Database query error" });
+                    return;
+                }
+                console.log('Farmer Statistics:', farmerStatistics);
+        // ดึงรายชื่อหมอพืชสำหรับ station นี้
+                const doctorQuery = `
+                    SELECT id_doctor, fullname_doctor, station_doctor
+                    FROM acc_doctor
+                    WHERE station_doctor = ?;
+                `;
+                con.query(doctorQuery, [station], (err, doctors) => {
+                    if (err) {
+                        console.error('Error fetching doctor data:', err);
+                        res.status(500).json({ status: "error", message: "Database query error" });
+                        return;
+                    }
+                    console.log('Doctors:', doctors);
+        // ส่งผลลัพธ์กลับไป
+                    res.status(200).json({
+                        status: "success",
+                        data: {
+                            farmerStatistics: farmerStatistics.map((stat) => ({
+                                station: stat.station,
+                                totalFarmers: stat.total_farmers,
+                                totalPlants: stat.total_plants,
+                                plants: stat.plants,
+                                plantDetails: JSON.parse(stat.plantDetails || "[]").reduce((prev , curr) => {
+                                    const indexFind = prev.findIndex(({ plantName }) => plantName === curr["plantName"])
+                                    if(indexFind >= 0) {
+                                        prev[indexFind]["farmersCount"] += curr["farmersCount"]
+                                    } else {
+                                        prev.push({
+                                            plantName : curr["plantName"],
+                                            farmersCount : curr["farmersCount"]
+                                        })
+                                    }
+                                    return prev
+                                } , []),
+                            })),
+                            doctors,
+                        },
+                    });
+                });
+            });
           }
         } catch (err) {
-            console.log(err)
           con.end()
           if(err == "not pass") {
             res.redirect('/api/logout')
