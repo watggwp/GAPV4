c849a3caeddcddad8afc1c92769c5a36d88a5a7e
thanawat1303 <fquarius13@gmail.com>
2025-02-05 23:28:12 +0700

    update group

--- server/apiAdmin.js
+++ server/apiAdmin.js
@@ -830,7 +830,7 @@ app.get('/api/admin/profile/get', (req, res) => {
           INNER JOIN pests AS p ON pc.pest_id = p.pest_id
           INNER JOIN chemical_list AS c ON pc.chemical_id = c.id
           INNER JOIN plant_list AS pl ON pc.plant_id = pl.id
-          `, [ search , search , search , search ] ,
+          `, [ `%${search}%` , `%${search}%` , `%${search}%` , `%${search}%` ] ,
           (err, results) => {
             if (err) {
               console.error("Database query error:", err);
