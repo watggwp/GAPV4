e4c4b36de964dd898952d47121e8c4146d7e905e
thanawat1303 <fquarius13@gmail.com>
2025-02-08 01:07:17 +0700

    context

--- app/src/web/doctor/src/page/group/InsertGroup.js
+++ app/src/web/doctor/src/page/group/InsertGroup.js
@@ -1,10 +1,10 @@
 import { useCallback, useContext, useEffect, useState } from "react";
 import { clientMo } from "../../../../../assets/js/moduleClient";
-import PageData from "../data/PageData";
+import { PageDataContext } from "../data/PageData";
  
 const InsertGroup = () => {
  
-    const { popupDataManage, setPopupDataManage } = useContext(PageData);
+    const { popupDataManage, setPopupDataManage } = useContext(PageDataContext);
     const [chemicals, setChemicals] = useState([]);
     const [pests, setPests] = useState([]);
     const [plants, setPlants] = useState([]);
