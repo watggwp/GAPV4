14718145a8d37afd4b57f69bb8f40baa2960fa98
thanawat1303 <fquarius13@gmail.com>
2025-02-08 21:33:06 +0700

    ยสฟืะ

--- app/src/web/admin/src/page/data/types/Plant.js
+++ app/src/web/admin/src/page/data/types/Plant.js
@@ -9,21 +9,36 @@ export default function Plant({ Data }) {
                 <TextField
                     label="ชื่อพืช"
                     variant="outlined"
-                    value={Data.name}
+                    defaultValue={Data.name}
+                    slotProps={{
+                        htmlInput : {
+                            readOnly : true
+                        }
+                    }}
                 />
             </Grid2>
             <Grid2 size={{ sm : 12 , md : 6 }}>
                 <TextField
                     label="ชนิดพืช"
                     variant="outlined"
-                    value={Data.type_plant}
+                    defaultValue={Data.type_plant}
+                    slotProps={{
+                        htmlInput : {
+                            readOnly : true
+                        }
+                    }}
                 />
             </Grid2>
-            <Grid2 size={{ sm : 12 , md : 6 }}>
+            <Grid2 size={{ xs : 12 }}>
                 <TextField
                     label="สายพันธ์ุพืช"
                     variant="outlined"
-                    value={Data.variety_name}
+                    defaultValue={Data.variety_name}
+                    slotProps={{
+                        htmlInput : {
+                            readOnly : true
+                        }
+                    }}
                 />
             </Grid2>
         </React.Fragment>
