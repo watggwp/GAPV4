78f1361e4bc954970e007193d63ad41531b349e5
thanawat1303 <fquarius13@gmail.com>
2025-02-09 19:40:27 +0700

    manage

--- app/src/web/admin/src/page/report/statistics/buttonChange.js
+++ app/src/web/admin/src/page/report/statistics/buttonChange.js
@@ -1,73 +1,23 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useCallback } from "react";
 import { PageTemplateContext } from "../../PageTemplate";
 import { Modal } from "react-bootstrap";
 import { InsertStatisticsContext } from "./InsertStatistics";
 import { clientMo } from "../../../../../../assets/js/moduleClient";
 
 export default function ButtonChangeStatistics() { 
-    const { minCount, selectedRows } = useContext(InsertStatisticsContext);
+    const { minCount } = useContext(InsertStatisticsContext);
     const { ChangeStatus } = useContext(PageTemplateContext);
     
     const [popupDataManage, setPopupDataManage] = useState({ open: false });
-    const [stateOnBt, setStateOnBt] = useState(true);
-    const [selectedData, setSelectedData] = useState([]);
-    const [chemicals, setChemicals] = useState({}); // เก็บข้อมูลสารเคมีที่ใช้
 
     // ดึงข้อมูลสารเคมีที่ใช้จาก API
-    useEffect(() => {
-        if (selectedRows.size) {
-            selectedRows.forEach(row => {
-                clientMo.post("/api/admin/chemical_pest/get", { pest_id: row.id }) 
-                    .then(response => {
-                        // console.log(response)
-                        const data = JSON.parse(response)
-                        if (data && data.chemical_used) {
-                            setChemicals(prevChemicals => ({
-                                ...prevChemicals,
-                                [row.id]: response.data.chemical_used // เก็บชื่อสารเคมีตาม pest_name
-                            }));
-                        }
-                    })
-                    .catch(error => console.error("Error fetching data:", error));
-            });
-        }
-    }, [selectedRows]);
-
-    const fetchStatistics = async () => {
-        try {
-            setSelectedData(() => {
-                const newSelectsData = [];
-                selectedRows.forEach(selectedRow => {
-                    newSelectsData.push(selectedRow);
-                });
-                console.log(newSelectsData);
-                return newSelectsData;
-            });
-
-            setPopupDataManage({ open: true });
-
-        } catch (error) {
-            console.error("เกิดข้อผิดพลาดในการโหลดข้อมูล:", error);
-        }
-    };
 
     const handleOpenModal = () => {
-        if (minCount >= 1) {
-            fetchStatistics();
-        }
+        setPopupDataManage({ open: true });
     };
 
     const handleCloseModal = () => {
         setPopupDataManage({ open: false });
-    };
-
-    const Cancel = () => {
-        handleCloseModal();
-    };
-
-    const ClickAdd = () => {
-        console.log("เพิ่มข้อมูลเรียบร้อยแล้ว!");
-        handleCloseModal(); 
     };
 
     return (
@@ -100,60 +50,125 @@ export default function ButtonChangeStatistics() {
                 centered
                 size="lg"
             >
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h2 id="modal-title">แจ้งเตือนการระบาด</h2>
-                        <button type="button" className="btn-close" onClick={handleCloseModal}></button>
-                    </div>
-
-                    <div className="modal-body" style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                        {selectedData.length > 0 ? (
-                            <table style={{ 
-                                width: "90%", 
-                                borderCollapse: "collapse", 
-                                marginTop: "10px", 
-                                border: "2px solid #60d6cf", 
-                                textAlign: "center"
-                            }}>
-                                <thead>
-                                    <tr style={{ backgroundColor: "#60d6cf", color: "white" }}>
-                                        <th style={{ padding: "10px", border: "1px solid white" }}>ชื่อโรค/ศัตรูพืช</th>
-                                        <th style={{ padding: "10px", border: "1px solid white" }}>พืชที่เกี่ยวข้อง</th>
-                                        <th style={{ padding: "10px", border: "1px solid white" }}>จำนวน</th>
-                                        <th style={{ padding: "10px", border: "1px solid white" }}>สารเคมีที่ใช้</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {[...selectedData]
-                                        .sort((a, b) => b.count - a.count) 
-                                        .map((item, index) => (
-                                            <tr key={index} style={{ backgroundColor: index % 2 === 0 ? "#f8f8f8" : "#fff" }}>
-                                                <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-                                                    {item.name || item.insect}
-                                                </td>
-                                                <td style={{ padding: "10px", border: "1px solid #ddd" }}>{item.name_plants}</td>
-                                                <td style={{ padding: "10px", border: "1px solid #ddd" }}>{item.count}</td>
-                                                <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-                                                    {chemicals[item.id] || "-"}
-                                                </td>
-                                            </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        ) : (
-                            <p style={{ textAlign: "center", color: "gray", fontWeight: "bold", padding: "10px" }}>ไม่มีข้อมูลที่จะแสดง</p>
-                        )}
-                    </div>
-                    <div className="bt-submitnoty">
-                        <button className="cancel" onClick={Cancel}>
-                            ยกเลิก
-                        </button>
-                        <button className="submit" onClick={ClickAdd} disabled={!stateOnBt}>
-                            แจ้งข้อมูล
-                        </button>
-                    </div>
-                </div>
+                <NotifyStatistics handleCloseModal={handleCloseModal} />
             </Modal>
         </div>
     );
+}
+
+function NotifyStatistics({
+    handleCloseModal
+}) {
+    const { minCount, selectedRows } = useContext(InsertStatisticsContext);
+    const [stateOnBt, setStateOnBt] = useState(true);
+    const [selectedData, setSelectedData] = useState([]);
+
+    const fetchStatistics = useCallback(async () => {
+        try {
+            setSelectedData(() => {
+                const newSelectsData = [];
+                selectedRows.forEach(selectedRow => {
+                    newSelectsData.push(selectedRow);
+                });
+                return newSelectsData;
+            });
+
+        } catch (error) {
+            console.error("เกิดข้อผิดพลาดในการโหลดข้อมูล:", error);
+        }
+    } , [selectedRows]);
+
+    const Cancel = () => {
+        handleCloseModal();
+    };
+
+    const ClickAdd = () => {
+        console.log("เพิ่มข้อมูลเรียบร้อยแล้ว!");
+        handleCloseModal(); 
+    };
+
+    useEffect(() => {
+        if(minCount >= 1) {
+            fetchStatistics()
+        }
+    }, [fetchStatistics , minCount]);
+
+    return(
+        <div className="modal-content">
+            <div className="modal-header">
+                <h2 id="modal-title">แจ้งเตือนการระบาด</h2>
+                <button type="button" className="btn-close" onClick={handleCloseModal}></button>
+            </div>
+
+            <div className="modal-body" style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+                {selectedData.length > 0 ? (
+                    <table style={{ 
+                        width: "90%", 
+                        borderCollapse: "collapse", 
+                        marginTop: "10px", 
+                        border: "2px solid #60d6cf", 
+                        textAlign: "center"
+                    }}>
+                        <thead>
+                            <tr style={{ backgroundColor: "#60d6cf", color: "white" }}>
+                                <th style={{ padding: "10px", border: "1px solid white" }}>ชื่อโรค/ศัตรูพืช</th>
+                                <th style={{ padding: "10px", border: "1px solid white" }}>พืชที่เกี่ยวข้อง</th>
+                                <th style={{ padding: "10px", border: "1px solid white" }}>จำนวน</th>
+                                <th style={{ padding: "10px", border: "1px solid white" }}>สารเคมีที่ใช้</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {[...selectedData]
+                                .sort((a, b) => b.count - a.count) 
+                                .map((item, index) => (
+                                    <NotifyStatistic key={index} index={index} item={item} />
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p style={{ textAlign: "center", color: "gray", fontWeight: "bold", padding: "10px" }}>ไม่มีข้อมูลที่จะแสดง</p>
+                )}
+            </div>
+            <div className="bt-submitnoty">
+                <button className="cancel" onClick={Cancel}>
+                    ยกเลิก
+                </button>
+                <button className="submit" onClick={ClickAdd} disabled={!stateOnBt}>
+                    แจ้งข้อมูล
+                </button>
+            </div>
+        </div>
+    )
+}
+
+function NotifyStatistic({
+    index ,
+    item
+}) {
+
+    const [chemical , setChemical] = useState("")
+
+    useEffect(() => {
+        clientMo.post("/api/admin/chemical_pest/get", { pest_id: item.id }) 
+            .then(response => {
+                const data = JSON.parse(response)
+                if (data && data.chemical_used) {
+                    setChemical(data.chemical_used);
+                }
+            })
+            .catch(error => console.error("Error fetching data:", error));
+    } , [item.id])
+
+    return(
+        <tr key={index} style={{ backgroundColor: index % 2 === 0 ? "#f8f8f8" : "#fff" }}>
+            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+                {item.name || item.insect}
+            </td>
+            <td style={{ padding: "10px", border: "1px solid #ddd" }}>{item.name_plants}</td>
+            <td style={{ padding: "10px", border: "1px solid #ddd" }}>{item.count}</td>
+            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+                {chemical || "-"}
+            </td>
+        </tr>
+    )
 }
