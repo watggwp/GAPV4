11740469cdb3699495d3b98f626867b6c243386f
thanawat1303 <fquarius13@gmail.com>
2025-02-17 22:39:06 +0700

    add router

--- server/apifunc.js
+++ server/apifunc.js
@@ -26,6 +26,7 @@ const apifunc = {
           `SELECT * FROM ${roleAuth} WHERE BINARY ${usernameDB} = ? AND ${passwordDB}=SHA2( ? , 256) ${ROLE} ${ORDER}`,
           [username, password],
           (err, result) => {
+            console.log(result)
             if (err) {
               // ErrorDB(connectDB, err, res);
               reject("not pass");
