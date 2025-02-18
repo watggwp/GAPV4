f01391e79f4916b47d8ded33197a4852e167c997
thanawat1303 <fquarius13@gmail.com>
2025-02-19 00:22:10 +0700

    messageing

--- server/appRun.js
+++ server/appRun.js
@@ -2,10 +2,7 @@ const appConfig = require("./configExpress")
 const fs = require('fs')
 module.exports = function appRun(username , password){
     console.log("Load Server...")
-    const UrlServer = (process.argv[2] == process.env.BUILD) ? 
-                        "https://" + process.env.REACT_APP_API_PUBLIC +":"+process.env.REACT_APP_API_PORT :
-                        JSON.parse(fs.readFileSync(__dirname.replace('\server' , "/UrlServer.json")).toString()).url
-    const app = appConfig(username , password , UrlServer) 
+    const app = appConfig(username , password , process.env.URL_SERVER) 
     const Port = parseInt(process.env.REACT_APP_API_PORT ?? "80")
     app.listen( Port , "0.0.0.0" , function () {
         console.log('Start on port '+Port+'!\n');
