47ae525f0dfffd56f6409002e5c111c17b42c3e4
วันชนะ เบ็ญจนาค <1164104001139@mail.rmutt.ac.th>
2025-03-15 14:12:49 +0700

    editadmin

--- app/src/web/admin/src/Login.js
+++ app/src/web/admin/src/Login.js
@@ -56,7 +56,7 @@ const Login = ({ setBodyFileMain, state = false, socket }) => {
                         setErrorMessage("ผู้ใช้บัญชีนี้ถูกลบ");
                         ErrorLogin.current.setAttribute("show", "");
                     } else {
-                        setErrorMessage("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง");
+                        setErrorMessage("รหัสประจำตัวผู้ดูแลระบบหรือรหัสผ่านไม่ถูกต้อง");
                         ErrorLogin.current.setAttribute("show", "");
                         for (let x = 0; x < e.target.length - 1; x++) {
                             let prevent = e.target[x].parentElement;
@@ -104,7 +104,7 @@ const Login = ({ setBodyFileMain, state = false, socket }) => {
                             <path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z"/>
                         </svg>
                     </span>
-                    <input autoComplete="off" id="username-admin-login" type="text" name="username" placeholder="ชื่อผู้ใช้" />
+                    <input autoComplete="off" id="username-admin-login" type="text" name="username" placeholder="รหัสประจำตัวผู้ดูแลระบบ" />
                 </label>
                 <label>
                     <span>
