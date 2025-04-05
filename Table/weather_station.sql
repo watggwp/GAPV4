1cab2ace0fd2ba41e32353bd842372023e8fe2be
COZY GUYHW <85698300+GUYBURA@users.noreply.github.com>
2025-04-05 18:01:07 +0700

    Add rainfal in API and Database

--- Table/weather_station.sql
+++ Table/weather_station.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2025 at 02:03 PM
+-- Generation Time: Apr 05, 2025 at 12:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,36 +34,18 @@ CREATE TABLE `weather_station` (
   `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
   `temperature` double DEFAULT NULL,
   `humidity` double DEFAULT NULL,
-  `light` double DEFAULT NULL
+  `light` double DEFAULT NULL,
+  `rainfall` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `weather_station`
 --
 
-INSERT INTO `weather_station` (`id`, `device_id`, `station_id`, `timestamp`, `temperature`, `humidity`, `light`) VALUES
-(1, 'weather-station-1', 2, '2025-03-24 00:40:00', 45, 70, 1000),
-(2, 'hel-lora-32', 1, '2025-03-24 18:18:14', 32.24, 34.56, 969),
-(3, 'hel-lora-32', 1, '2025-03-24 18:18:29', 26.92, 74.83, 866),
-(4, 'hel-lora-32', 1, '2025-03-24 18:19:29', 25.11, 76.81, 395),
-(5, 'hel-lora-32', 1, '2025-03-24 18:20:15', 30.63, 59.72, 174),
-(6, 'hel-lora-32', 1, '2025-03-24 18:20:31', 25.87, 63.38, 609),
-(7, 'hel-lora-32', 1, '2025-03-24 18:21:31', 21.61, 74.85, 639),
-(8, 'hel-lora-32', 1, '2025-03-24 18:22:32', 30.09, 50.6, 683),
-(9, 'hel-lora-32', 1, '2025-03-24 18:22:46', 34.54, 34.86, 267),
-(10, 'hel-lora-32', 1, '2025-03-24 18:23:33', 26.55, 79.72, 466),
-(11, 'hel-lora-32', 1, '2025-03-24 18:24:19', 20.18, 53.02, 7),
-(12, 'hel-lora-32', 1, '2025-03-24 18:24:35', 32.14, 49.45, 541),
-(13, 'hel-lora-32', 1, '2025-03-24 18:25:36', 25.67, 43.39, 544),
-(14, 'hel-lora-32', 1, '2025-03-24 18:25:51', 24.7, 55.16, 509),
-(15, 'hel-lora-32', 1, '2025-03-24 18:26:23', 23.84, 75.25, 715),
-(16, 'hel-lora-32', 1, '2025-03-24 18:26:37', 20.57, 77.88, 881),
-(17, 'hel-lora-32', 1, '2025-03-24 18:26:52', 29.4, 48.68, 263),
-(18, 'hel-lora-32', 1, '2025-03-24 18:27:37', 21.76, 42.72, 331),
-(19, 'hel-lora-32', 1, '2025-03-24 18:28:38', 30.43, 65.54, 519),
-(20, 'hel-lora-32', 1, '2025-03-24 19:14:32', 29.94, 71.11, 744),
-(21, 'hel-lora-32', 1, '2025-03-24 19:14:46', 32.97, 35.66, 818),
-(22, 'hel-lora-32', 1, '2025-03-24 19:15:22', 21.6, 51.98, 607);
+INSERT INTO `weather_station` (`id`, `device_id`, `station_id`, `timestamp`, `temperature`, `humidity`, `light`, `rainfall`) VALUES
+(26, 'weather-station-1', 2, '2025-03-24 00:40:00', 45, 70, 1000, 200),
+(28, 'weather-station-1', 2, '2025-03-23 17:40:00', 45, 70, 1000, 200),
+(34, 'hel-lora-32', 1, '2025-04-05 10:54:09', 5.52, 137.13, 13897, 49.78);
 
 --
 -- Indexes for dumped tables
@@ -84,7 +66,7 @@ ALTER TABLE `weather_station`
 -- AUTO_INCREMENT for table `weather_station`
 --
 ALTER TABLE `weather_station`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- Constraints for dumped tables
