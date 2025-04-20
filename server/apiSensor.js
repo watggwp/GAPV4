9c3dcec0304dd46865ee6db39586965cd9999fc1
thanawat1303 <fquarius13@gmail.com>
2025-04-20 23:32:04 +0700

    api

--- server/apiSensor.js
+++ server/apiSensor.js
@@ -36,46 +36,52 @@ module.exports = function apiSensor(app, connectionDB = new Pool()) {
 	app.post("/api/sensor/weather-greenhouse", async (req, res) => {
         // แสดงข้อมูลที่ TTN ส่งมาใน console
         console.log("📥 Received TTN data:", req.body);
-        const {
-			end_device_ids : {
-				device_id
-			},
-            uplink_message : {
-				decoded_payload : {
-					humidity_air,
-					humidity_soil,
-					light,
-					temperature_air,
-					temperature_soil
-				}
-			},
-			received_at : timestamp
-        } = req.body
+        try {
+			const {
+				end_device_ids : {
+					device_id
+				},
+				uplink_message : {
+					decoded_payload : {
+						humidity_air,
+						humidity_soil,
+						light,
+						temperature_air,
+						temperature_soil
+					}
+				},
+				received_at : timestamp
+			} = req.body
 
-		try {
-			await connectionDB.executeQuery(
-				`
-					INSERT INTO weather_greenhouse
-						(device_id, timestamp, air_temperature, air_humidity, light, soil_temperature, soil_humidity)
-					VALUES
-						(?, ?, ?, ?, ?, ?, ?)
-				`,
-				[
-					device_id,
-					timestamp,
-					temperature_air,
-					humidity_air,
-					light,
-					temperature_soil,
-					humidity_soil
-				]
-			)
-
-			console.log("✅ Inserted to DB:", result.insertId);
-			res.status(200).send("success");
+			try {
+				await connectionDB.executeQuery(
+					`
+						INSERT INTO weather_greenhouse
+							(device_id, timestamp, air_temperature, air_humidity, light, soil_temperature, soil_humidity)
+						VALUES
+							(?, ?, ?, ?, ?, ?, ?)
+					`,
+					[
+						device_id,
+						timestamp,
+						temperature_air,
+						humidity_air,
+						light,
+						temperature_soil,
+						humidity_soil
+					]
+				)
+	
+				console.log("✅ Inserted to DB:", result.insertId);
+				res.status(200).send("success");
+			} catch(err) {
+				console.error("❌ DB Error:", err);
+				res.status(500).send("Failed to insert sensor data");
+			}
+			
 		} catch(err) {
-			console.error("❌ DB Error:", err);
-			res.status(500).send("Failed to insert sensor data");
+			console.error("❌ Error parsing TTN data:", err);
+			return res.status(400).send("Invalid data format");
 		}
     });
 }
