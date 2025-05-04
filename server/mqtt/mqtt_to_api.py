77bfde53f96d8075804deb9d1b59142d4c64b475
PeeraphatN <contactpeeraphat.n@gmail.com>
2025-05-04 10:38:01 +0700

    mqtt

--- server/mqtt/mqtt_to_api.py
+++ server/mqtt/mqtt_to_api.py
@@ -16,39 +16,45 @@ with open("device_config.json") as f:
 
 
 def on_message(client, userdata, msg):
-    try:
+    try :
         payload = json.loads(msg.payload.decode())
+        try:
+            device_id = payload["end_device_ids"]["device_id"]
+            timestamp = payload["received_at"]
+            timestamp_fixed = timestamp[:26] + "Z"
+            dt_utc = datetime.strptime(timestamp_fixed, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=pytz.utc)
+            dt_bangkok = dt_utc.astimezone(pytz.timezone("Asia/Bangkok"))
 
-        device_id = payload["end_device_ids"]["device_id"]
-        timestamp = payload["received_at"]
-        timestamp_fixed = timestamp[:26] + "Z"
-        dt_utc = datetime.strptime(timestamp_fixed, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=pytz.utc)
-        dt_bangkok = dt_utc.astimezone(pytz.timezone("Asia/Bangkok"))
+            config = DEVICE_CONFIG_MAP.get(device_id)
+            if not config:
+                print(f"{timestamp} ไม่พบ config สำหรับอุปกรณ์: {device_id}")
+                return
 
-        config = DEVICE_CONFIG_MAP.get(device_id)
-        if not config:
-            print(f"ไม่พบ config สำหรับอุปกรณ์: {device_id}")
-            return
+            data = {
+                "device_id": device_id,
+                "timestamp": dt_bangkok.isoformat()
+            }
 
-        data = {
-            "device_id": device_id,
-            "timestamp": dt_bangkok.isoformat()
-        }
+            decoded = payload["uplink_message"]["decoded_payload"]
 
-        decoded = payload["uplink_message"]["decoded_payload"]
+            for field in config["fields"]:
+                value = decoded.get(field)
+                if value is not None:
+                    data[field] = value
 
-        for field in config["fields"]:
-            value = decoded.get(field)
-            if value is not None:
-                data[field] = value
+            print(json.dumps(data, indent=2) , decoded)
 
-        print(json.dumps(data, indent=2) , decoded)
+            response = requests.post(config["api"], json=data)
+            print(f"ส่งไปยัง API: {config['api']} → {response.status_code} - {response.text}")
 
-        response = requests.post(config["api"], json=data)
-        print(f"ส่งไปยัง API: {config['api']} → {response.status_code} - {response.text}")
+        except Exception as e:
+            print("Error:", payload , e)
+    except Exception as errJsonLoad :
+        try :
+            print("Error json load: ", errJsonLoad , msg.payload.decode())
+        except Exception as errDecode :
+            print("Error decode: " , errDecode)
 
-    except Exception as e:
-        print("Error:", e)
 
 # === Setup MQTT ===
 client = mqtt.Client()
