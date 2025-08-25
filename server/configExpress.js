e239968f1bf823b80892df987423c4e075f59f28
COZY GUYHW <85698300+GUYBURA@users.noreply.github.com>
2025-08-25 13:17:13 +0700

    Add api add device from GAP Dashboard

--- server/configExpress.js
+++ server/configExpress.js
@@ -35,12 +35,13 @@ const apiSchedules = require('./endpoints/schedules');
 const apiFertilizers = require('./endpoints/fertilizer');
 const apiChemicals = require('./endpoints/chemical');
 const apiPests = require('./endpoints/pest');
+const apiAddDevice = require('./apiAddDevice');
 const callServices = require('./callServices');
 
 
-module.exports = function appConfig(username , password , UrlNgrok ) {
+module.exports = function appConfig(username, password, UrlNgrok) {
     require('dotenv').config().parsed
- 
+
     // const python = spawn(
     //     'python', ['./server/mqtt/mqtt_to_api.py'] , {
     //         env : {
@@ -66,33 +67,33 @@ module.exports = function appConfig(username , password , UrlNgrok ) {
         password: password,
         mode: mode
     })
-    
+
     // เมื่อใช้ ngrok หากไม่ได้ใช้ ngrok ให้ comment
     app.set('trust proxy', 1);
 
     const upload = multer()
     const server =
-                // (mode == process.env.BUILD) ? https.createServer({
-                //     key: fs.readFileSync(process.env.pathCertFile),
-                //     cert: fs.readFileSync(process.env.pathKeyFile)
-                // } , app)
-                // :
-                http.createServer(app)
+        // (mode == process.env.BUILD) ? https.createServer({
+        //     key: fs.readFileSync(process.env.pathCertFile),
+        //     cert: fs.readFileSync(process.env.pathKeyFile)
+        // } , app)
+        // :
+        http.createServer(app)
     // set Server
- 
-    const listDB = dbpackage.listConfig(username , password , mode)
+
+    const listDB = dbpackage.listConfig(username, password, mode)
     // const HOST_CHECK = (mode == process.env.BUILD) ? process.env.REACT_APP_API_PUBLIC : process.env.REACT_APP_API_LOCAL;
     // config server and Hot Refresh
     // if(mode != process.env.BUILD) reactServ(app)
 
     ScheduleCorn(Pool)
-    callServices(app , Pool)
+    callServices(app, Pool)
 
     // set session
     console.log(mode)
     const sessionMiddleware = sessions({
-        name : process.env.cookie,
-        secret : process.env.KEY_SESSION ?? "gap_project_royal",
+        name: process.env.cookie,
+        secret: process.env.KEY_SESSION ?? "gap_project_royal",
         saveUninitialized: false,
         // cookie: {
         //     httpOnly: true,
@@ -103,12 +104,12 @@ module.exports = function appConfig(username , password , UrlNgrok ) {
         // resave : false
         cookie: {
             httpOnly: true,
-            secure : mode == process.env.BUILD,
+            secure: mode == process.env.BUILD,
             maxAge: null,
             sameSite: 'strict'
             // secure: mode != process.env.BUILD ? false : true
         },
-        resave : false
+        resave: false
     })
 
     // set proxy
@@ -120,40 +121,40 @@ module.exports = function appConfig(username , password , UrlNgrok ) {
             return path
         }
     }))
-   
+
     // app.use(express.json())
     app.use(cookieParser())
     app.use(sessionMiddleware)
     app.use(express.json({ limit: "50mb" }));
     app.use(express.urlencoded({ limit: "50mb", extended: true }));
-    
-    const jsonDataNgrok = JSON.parse(fs.readFileSync(__dirname.replace('\server' , "/UrlServer.json")).toString())
+
+    const jsonDataNgrok = JSON.parse(fs.readFileSync(__dirname.replace('\server', "/UrlServer.json")).toString())
     const origins = [
-        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.REACT_APP_API_PORT}`, 
-        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.ADMIN_PORT}`, 
-        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.DOCTOR_PORT}`, 
-        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.FARMER_PORT}`, 
-        ...Object.entries(jsonDataNgrok).map((Data)=>Data[1]), 
+        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.REACT_APP_API_PORT}`,
+        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.ADMIN_PORT}`,
+        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.DOCTOR_PORT}`,
+        `http://${process.env.REACT_APP_API_LOCAL}:${process.env.FARMER_PORT}`,
+        ...Object.entries(jsonDataNgrok).map((Data) => Data[1]),
         `https://${process.env.REACT_APP_API_PUBLIC}:${process.env.REACT_APP_API_PORT}`,
-        "https://gapv2.ngrok.app"
+        "https://gapv2.ngrok.app",
     ]
-    
+
     // protocal websocket
-    const io = WebSocket(server , sessionMiddleware , origins , db , listDB , apifunc)
+    const io = WebSocket(server, sessionMiddleware, origins, db, listDB, apifunc)
 
     app.use(cors({
-        origin : origins,
+        origin: origins,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     }))
 
     // middleware custom
     app.use(logging(Pool))
-    app.use("/api/schedules" , authorizer(Pool))
-    app.use("/api/schedules/*" , authorizer(Pool))
-    app.use("/api/pests" , authorizer(Pool))
-    app.use("/api/pests/*" , authorizer(Pool))
-    
+    app.use("/api/schedules", authorizer(Pool))
+    app.use("/api/schedules/*", authorizer(Pool))
+    app.use("/api/pests", authorizer(Pool))
+    app.use("/api/pests/*", authorizer(Pool))
+
     // config environment
     app.use(upload.any())
     app.use(express.static('app/src/assets/style'))
@@ -161,9 +162,9 @@ module.exports = function appConfig(username , password , UrlNgrok ) {
     app.use(express.static('app/src/assets/img'))
     app.use(express.static('app/src/assets/js'))
     app.use(express.static('app/src/assets/icon'))
- 
-    app.get('/' , (req, res) => {
-        res.sendFile(__dirname.replace('\server' , '/index404.html'));
+
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname.replace('\server', '/index404.html'));
     });
 
     app.use(express.static('build/admin'))
@@ -171,25 +172,26 @@ module.exports = function appConfig(username , password , UrlNgrok ) {
     app.use(express.static('build/farmer'))
 
     // router api url
-    if(mode === process.env.BUILD || mode === "router") router(app)
-    apiAdmin(app , db , Pool , apifunc , dbpackage , listDB , io)
-    apiDoctor(app , db , Pool , apifunc , dbpackage , listDB , UrlNgrok , io)
-    apiFarmer(app , db , Pool , dbpackage , listDB , io)
-    apiEcph(app , Pool)
-    apiWeatherStation(app , Pool)
-    apiWeatherGreenhouse(app , Pool)
-    apiPump(app , Pool)
-    apiMessage(app , db , apifunc , dbpackage , listDB , UrlNgrok , io)
-    
-    apiSchedules(app , Pool)
-    apiFertilizers(app , Pool)
-    apiChemicals(app , Pool)
-    apiPests(app , Pool)
-    
+    if (mode === process.env.BUILD || mode === "router") router(app)
+    apiAdmin(app, db, Pool, apifunc, dbpackage, listDB, io)
+    apiDoctor(app, db, Pool, apifunc, dbpackage, listDB, UrlNgrok, io)
+    apiFarmer(app, db, Pool, dbpackage, listDB, io)
+    apiEcph(app, Pool)
+    apiWeatherStation(app, Pool)
+    apiWeatherGreenhouse(app, Pool)
+    apiPump(app, Pool)
+    apiMessage(app, db, apifunc, dbpackage, listDB, UrlNgrok, io)
+
+    apiSchedules(app, Pool)
+    apiFertilizers(app, Pool)
+    apiChemicals(app, Pool)
+    apiPests(app, Pool)
+
+    apiAddDevice(app, Pool)
     // page error 404
-    app.get("*" , (req, res) => {
-        res.sendFile(__dirname.replace('\server' , '/index404.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(__dirname.replace('\server', '/index404.html'));
     });
- 
+
     return server
 }
