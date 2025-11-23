bc8e0e450e0c7b818dae6f37eb0dc11984b00bce
thanawat1303 <fquarius13@gmail.com>
2025-11-23 21:54:55 +0700

    start-production

--- start-production.sh
+++ start-production.sh
@@ -1,7 +1,4 @@
 npm run server
-npm run server-services:gap-dashboard
 
-# check gap-backend
-# ถ้าขึ้นคำว่า NGROK Connected url: <url app> และไม่พบ error ถือว่าทำงานปกติ
-npm run server-logs
-npm run server-services:gap-dashboard-logs
+cd ./services/gap-device-dashboard/backend
+npm run server
