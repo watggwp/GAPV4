6bd7cbcca949a4da265f35141f3ce191e951969b
Lxmon899 <masterfamilytv@gmail.com>
2026-06-07 17:55:55 +0700

    ใช้ configExpress จาก main แทนของตัวเอง

--- server/configExpress.js
+++ server/configExpress.js
@@ -100,23 +100,23 @@ module.exports = function appConfig(username, password, hostServer) {
         name: process.env.cookie,
         secret: process.env.KEY_SESSION ?? "gap_project_royal",
         saveUninitialized: false,
-        cookie: {
-            httpOnly: true,
-            secure: isProduction,
-            // maxAge: 1000 * 60 * 60 * 24,
-            maxAge: null,
-            sameSite: isProduction ? "none" : "lax"
-        },
-        resave: false
         // cookie: {
-        //     // ตั้งให้เปิด mode https ได้
-        //     // httpOnly: true,
-        //     // secure : mode == process.env.BUILD,
-        //     maxAge: 1000 * 60 * 60 * 8, // 8 ชั่วโมง
-        //     sameSite: 'strict'
-        //     // secure: mode != process.env.BUILD ? false : true
+        //     httpOnly: true,
+        //     secure: true,
+        //     // maxAge: 1000 * 60 * 60 * 24,
+        //     maxAge: null,
+        //     sameSite: "none"
         // },
         // resave: false
+        cookie: {
+            // ตั้งให้เปิด mode https ได้
+            // httpOnly: true,
+            // secure : mode == process.env.BUILD,
+            maxAge: 1000 * 60 * 60 * 8, // 8 ชั่วโมง
+            sameSite: 'strict'
+            // secure: mode != process.env.BUILD ? false : true
+        },
+        resave: false
     })
 
     const subpathPrefix = process.env.PREFIX_PATH || ""; // ทุก redirect จะเพิ่ม prefix นี้
@@ -235,7 +235,7 @@ module.exports = function appConfig(username, password, hostServer) {
     app.use(express.static('build/farmer'))
 
     // services
-    ScheduleCorn(Pool)
+    ScheduleCorn(Pool, io)
     callServices(app, Pool)
 
     // router api url
