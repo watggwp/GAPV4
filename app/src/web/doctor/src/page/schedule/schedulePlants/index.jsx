337d775d25f4a08661d632d3f68b7e3cd71be7fa
Lxmon899 <masterfamilytv@gmail.com>
2026-06-18 17:56:05 +0700

    add mobile support for schedulePlants page

--- app/src/web/doctor/src/page/schedule/schedulePlants/index.jsx
+++ app/src/web/doctor/src/page/schedule/schedulePlants/index.jsx
@@ -1,20 +1,15 @@
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
 import SchedulePlantManagement from "../../../../../../assets/components/schedule-management/schedulePlants";
 import { useDoctor } from "../../../Doctor";
-import { useCallback } from "react";
 
 export default function SchedulePlants() {
     const { plant_id } = useParams()
     const { profile } = useDoctor()
-    const navigator = useNavigate()
-
-    const onBack = useCallback(() => navigator("/doctor/schedules"), [navigator])
 
     return (
         <SchedulePlantManagement
             plant_id={plant_id}
             station_id={profile?.station_doctor}
-            onBack={onBack}
         />
     )
 }
